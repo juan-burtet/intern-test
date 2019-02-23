c99d353263e393ed136b287cb5544d52a5128180
Juan Burtet <juan.burtet@gmail.com>
2019-02-22 23:41:16 -0300

    Consegue a quantidade de artigos do xml

--- myapp/app/controllers/extractor_controller.rb
+++ myapp/app/controllers/extractor_controller.rb
@@ -1,6 +1,12 @@
 class ExtractorController < ApplicationController
-	def teste
-		puts 'OI'
+	def adicionaPesquisador
+		# Quantidade de Artigos
+		qtArtigos = 0
+		doc = Nokogiri::XML(File.open("files/researcher_example.xml"))
+		doc.xpath('//ARTIGO-PUBLICADO').each do qtArtigos += 1
+		end
+
+		Teste.create({nome:'Cleide Viviane Buzanello Martins', qtArtigos:qtArtigos})
 	end 
 
 end
